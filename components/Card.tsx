05da635131f99da2ed67b3385942d8049dd54724
ApoorvaAman <apoorvaamankvs@gmail.com>
2021-07-31 23:30:33 +0530

    ✔ Changes in Card styles and tags

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,16 +6,20 @@ type CardInfo = {
 
 const Card = ({ title, desc, tags }: CardInfo): JSX.Element => {
   return (
-    <div className='p-3 bg-card shadow-md relative flex flex-col place-content-between gap-2'>
+    <div className='p-3 border-solid border-card border-2 rounded-md flex flex-col place-content-between gap-2'>
       <div>
-        <h5 className='text-lg font-sans font-bold text-primary'>{title}</h5>
-        <br />
+        <h5 className='text-lg font-sans font-bold text-primary mb-3'>
+          {title}
+        </h5>
         <p className='text-base font-geo text-body'>{desc}</p>
       </div>
       <div className='flex gap-2 flex-wrap max-w-max'>
         {tags.map((tag, i) => {
           return (
-            <span key={i} className='text-xs text-secondary text-geo'>
+            <span
+              key={i}
+              className='text-xs text-secondary text-geo bg-card rounded-xl pl-2 pr-2 pt-1 pb-1'
+            >
               {tag}
             </span>
           )
