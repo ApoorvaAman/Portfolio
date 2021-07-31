2945290ae4a1fe02216bcf81eb83043083cbcb87
ApoorvaAman <apoorvaamankvs@gmail.com>
2021-07-31 18:57:31 +0530

    ✔ More accurate type name

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,20 +1,21 @@
-type Card = {
+type CardInfo = {
   title: string
   desc: string
   tags: string[]
 }
 
-const Card = ({ title, desc, tags }: Card): JSX.Element => {
+const Card = ({ title, desc, tags }: CardInfo): JSX.Element => {
   return (
-    <div className='p-3 bg-card shadow-md relative'>
-      <h5 className='text-lg font-sans font-bold text-primary'>{title}</h5>
-      <br />
-      <p className='text-base font-sans text-body'>{desc}</p>
-      <br />
-      <div className='flex gap-2 flex-wrap max-w-max absolute bottom-2'>
+    <div className='p-3 bg-card shadow-md relative flex flex-col place-content-between gap-2'>
+      <div>
+        <h5 className='text-lg font-sans font-bold text-primary'>{title}</h5>
+        <br />
+        <p className='text-base font-geo text-body'>{desc}</p>
+      </div>
+      <div className='flex gap-2 flex-wrap max-w-max'>
         {tags.map((tag, i) => {
           return (
-            <span key={i} className='text-sm text-secondary'>
+            <span key={i} className='text-xs text-secondary text-geo'>
               {tag}
             </span>
           )
