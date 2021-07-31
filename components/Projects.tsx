b8ac28276bad223189faf334906c6c5c09a93e4d
ApoorvaAman <apoorvaamankvs@gmail.com>
2021-07-31 23:32:06 +0530

    ➕ Margins for different screen

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,11 @@
 import Card from '../components/Card'
+
 const Projects = (): JSX.Element => {
   return (
-    <section className='m-5'>
-      <h3 className='text-primary font-ubuntu font-bold text-3xl'>Projects</h3>
+    <section className='m-5 sm:ml-10 sm:mr-10 md:ml-14 md:mr-14'>
+      <h3 className='text-primary font-ubuntu font-bold text-3xl text-center'>
+        Projects
+      </h3>
       <div className='mt-9 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
         <Card
           title={'Discord Bot'}
