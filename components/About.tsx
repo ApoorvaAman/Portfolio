4707c1560c01aa3f632f57eb7167720e5bd50be0
ApoorvaAman <apoorvaamankvs@gmail.com>
2021-08-04 15:22:57 +0530

    👨‍💻 Added About Me section

---
+++ components/About.tsx
@@ -0,0 +1,21 @@
+const About = (): JSX.Element => {
+  return (
+    <section className='m-5 sm:ml-10 sm:mr-10 md:ml-14 md:mr-14'>
+      <h3 className='text-primary font-ubuntu font-bold text-3xl text-center'>
+        About Me
+      </h3>
+      <div>
+        <p>
+          I am Apoorva Aman, a frontend developer based in Patna, Bihar, India.
+        </p>
+        <p>
+          I am a graduate in Computer Science & Engineering. I enjoy building
+          websites and web apps. I use React and TypeScript. I am learning web
+          design as well.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Projects from '../components/Projects'
 import Intro from '../components/Intro'
+import About from '../components/About'
 
 const Home = (): JSX.Element => {
   return (
     <>
       <Intro />
       <Projects />
+      <About />
     </>
   )
 }
