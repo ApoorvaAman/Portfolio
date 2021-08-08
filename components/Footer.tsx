af0167fb9fa12bf41066831a7c81a3520f3e1e57
ApoorvaAman <apoorvaamankvs@gmail.com>
2021-08-08 15:22:47 +0530

    ⚫ Another footer for source code link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,23 @@
-const Footer = (): JSX.Element => {
-  const date = new Date()
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+const Footer = (): JSX.Element => {
   return (
-    <footer className='bg-body text-white mt-24 text-center'>
-      Copyright &copy; {date.getFullYear()} Apoorva Aman
-    </footer>
+    <>
+      <footer className='mt-24 mb-5 text-center text-geo'>
+        <a
+          className='bg-github text-white text-base pt-1 pb-1 pl-2 pr-2 rounded'
+          href='https://github.com/ApoorvaAman/Portfolio'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FontAwesomeIcon icon={faGithub} /> View source code on Github
+        </a>
+      </footer>
+      <footer className='bg-body text-white text-center p-3 text-geo'>
+        Copyright &copy; 2021 Apoorva Aman
+      </footer>
+    </>
   )
 }
 
