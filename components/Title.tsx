41e2314965301d9ca2cca3b682850bfbb88f4a32
ApoorvaAman <apoorvaamankvs@gmail.com>
2021-11-18 18:58:58 +0530

    Added Page Title

---
+++ components/Title.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const Title = (): JSX.Element => {
+  return (
+    <div>
+      <Head>
+        <title>Apoorva Aman | Portfolio</title>
+      </Head>
+    </div>
+  )
+}
+
+export default Title
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import Intro from '../components/Intro'
 import About from '../components/About'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
+import Title from '../components/Title'
 
 const Home = (): JSX.Element => {
   return (
     <>
+      <Title />
       <Intro />
       <Projects />
       <About />
